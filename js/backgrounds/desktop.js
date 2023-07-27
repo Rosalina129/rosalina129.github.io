67645ea3723b48904856e8c0d3b7bed5fe3c5da6
BeeUser12 <rosalina129@outlook.com>
2023-07-28 00:33:54 +0800

    Commit

--- js/backgrounds/desktop.js
+++ js/backgrounds/desktop.js
@@ -1,5 +1,6 @@
 var site_background = [
-    'https://static.zerochan.net/Collei.full.3940356.jpg'
+    ['https://static.zerochan.net/Collei.full.3940356.jpg','top center']
 ]
 
-document.body.style.backgroundImage = "url("+ site_background[0] +")";
+document.body.style.backgroundImage = "url("+ site_background[0][0] +")";
+document.body.style.backgroundPosition = site_background[0][1];
