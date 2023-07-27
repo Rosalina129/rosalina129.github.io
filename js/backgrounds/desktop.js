46b00d1b57ac4d4dcd6df54fe68189a7026a7fd8
BeeUser12 <rosalina129@outlook.com>
2023-07-28 00:51:33 +0800

    Commit

--- js/backgrounds/desktop.js
+++ js/backgrounds/desktop.js
@@ -2,7 +2,7 @@ var site_background = [
     ['https://static.zerochan.net/Collei.full.3940356.jpg','top center'],
     ['https://static.zerochan.net/Collei.full.3701452.jpg','top center'],
     ['https://static.zerochan.net/Collei.full.3949550.jpg','top center'],
-    ['https://static.zerochan.net/Collei.full.3887960.jpg','top center']
+    ['https://static.zerochan.net/Collei.full.3887960.png','top center']
 ]
 var a = Math.floor(Math.random()*4);
 document.body.style.backgroundImage = "url("+ site_background[a][0] +")";
