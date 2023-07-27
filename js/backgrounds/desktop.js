4af38e29757f5bade7bf902b3be49750d369be55
BeeUser12 <rosalina129@outlook.com>
2023-07-28 00:26:32 +0800

    Commit

---
+++ js/backgrounds/desktop.js
@@ -0,0 +1,5 @@
+var site_background = [
+    'images/background/collei/Collei.full.3887960.png'
+]
+
+document.body.style.backgroundImage = "url("+ site_background[0] +")";
--- js/cardcomp/cardcomp.js
+++ js/cardcomp/cardcomp.js
@@ -1,23 +1,86 @@
-function cardData (id,title,describe,siteurl,version){
+function cardData (id,title,describe,siteurl,version,islight,bimage){
     this.id = id
     this.title = title
     this.describe = describe
     this.siteurl = siteurl
-    this.version = this.version
-    return [this.id,this.title,this.describe,this.siteurl,this.version,this]
+    this.version = version
+    this.islight = islight
+    this.bimage = bimage
 };
-var cardAllData = [
-    new cardData(
-        "yave-tours",
-        "Yave's Tours",
-        'In this WinForms, you will play as a person named "Yave Yu" and upgrade through AFK! <br />Latest: v0.0.1',
-        new URL('https://github.com/Rosalina129/Yave'),
-        'Not Released'
-    )
+var CardData = [
+    [
+        new cardData(
+            "yave-tours",
+            "Yave's Tours",
+            '在这个由 C# WinForms 游戏中，您将扮演一个名为 Yave Yu 的人，通过不断的挂机进行升级!',
+            new URL('https://github.com/Rosalina129/Yave'),
+            'Not Released',
+            true,
+            ""
+        ),
+        new cardData(
+            "exps",
+            "乱码字生成器",
+            '由 C# 编写，拿来对文字进行“p̵̧̭̟͈̲̮̯̓̀͆͂ͨ͆̕͝ȓ̸͍̹͇͍̻̭̤̍̈́͊͢͢͢͠o̶̴̧̪̥͙͎̳̤̹̣̓̓̍̈́͞c̴̩̺͍̘͕͓̅ͧ̏̈͒̑ͥ̄͑ë̸̢̛̞̗̀͋͛͏̺̹̩̄̔́s̢̨͚̱̃̊ͩ̔̌̂ͤ́ͪ̚̕͝s̛̥̱͚͙͉̰͇͑̈́́ͤ̒̃̽̀”吧！',
+            new URL('https://github.com/Rosalina129/Exps'),
+            'v1.0.1',
+            false,
+            ""
+        )
+    ],[
+        new cardData(
+            "hodgepodge-2",
+            "Battle Hodgepodge 2",
+            '操控马力欧，打倒一波接着一波的敌人吧！',
+            new URL('https://github.com/Rosalina129/hodgepodge2'),
+            'v1.0',
+            true,
+            'images/hodgepodge2.png'
+        ),
+        new cardData(
+            "smbxware",
+            "SMBX Ware",
+            '在这个特定目标的地方尽可能存活到最后！',
+            new URL('https://github.com/Rosalina129/smbxware'),
+            'v0.0.1_alpha',
+            true,
+            'images/smbxware.png'
+        ),
+        new cardData(
+            "greentop",
+            "Green Top",
+            '在绿地里，操控马力欧收集金币的同时，收集散落在各地的游戏币吧！',
+            new URL('https://github.com/Rosalina129/cod_green_top'),
+            'v1.0',
+            true,
+            'images/greentop.png'
+        )
+    ]
 ]
-var cardStruct = '<div class="mdl-card mdl-shadow--4dp"><div class="mdl-card__title mdl-card--expand light"><p class="mdl-card__title-text" style="font-size: 1.5em;"></p></div><div class="mdl-card__supporting-text"></div><div class="mdl-card__actions mdl-card--border"><a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"></a><a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"></a></div></div>'
-
-console.log(cardAllData)
-
-const octokit = new Octokit({});
-await octokit.request("get /octocat", {});
+function cardStruct(id,title,describe,siteurl,version,islight,bimage) {
+    var colormode,background_url;
+    if (islight) colormode = "light"
+    else colormode = ""
+    if (bimage != "") background_url = 'style="background:url('+ bimage+')"'
+    return '<div class="mdl-card mdl-shadow--4dp" id="' + 
+    id + '"><div class="mdl-card__title mdl-card--expand "'+
+    background_url +'><div class="card-included-image"><p class="mdl-card__title-text" style="font-size: 1.5em;">' + 
+    title+ '</p></div></div><div class="mdl-card__supporting-text"><p>' + 
+    describe+ '</p><p>最新版本: ' + version +'</p></div><div class="mdl-card__actions mdl-card--border"><a href="' + 
+    siteurl.href + '/releases' + '" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">下载</a><a href="' + 
+    siteurl.href + '"class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">源代码</a></div></div>'
+}
+var CardQuery = [document.querySelector('.allapp-cards'),document.querySelector('.smbx-content-cards')];
+for (var i = 0; i < CardQuery.length; i++) {
+    for (var j = 0; j < CardData[i].length; j++) {
+        CardQuery[i].innerHTML += cardStruct(
+            CardData[i][j].id,
+            CardData[i][j].title,
+            CardData[i][j].describe,
+            CardData[i][j].siteurl,
+            CardData[i][j].version,
+            CardData[i][j].islight,
+            CardData[i][j].bimage
+        )
+    }
+}
